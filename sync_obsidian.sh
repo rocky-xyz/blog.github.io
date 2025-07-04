96f9d0a8f7056c96d5a81a96aed058f1bc07cd8d
rocky <rocky.xyzhou@gmail.com>
2025-07-05 04:19:03 +1000

    Update sync_obsidian.sh

--- sync_obsidian.sh
+++ sync_obsidian.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Sync Obsidian notes and imgs..."
-cp /Users/rocky/School/Obsidian/9517/notes.md ./_posts/comp9517.md
-rsync -av --delete /Users/rocky/School/Obsidian/9517/images ./assets/images/comp9517/
+cp /Users/rocky/School/Obsidian/github-blog/9517/notes.md ./_posts/comp9517.md
+cp /Users/rocky/School/Obsidian/github-blog/pandas/notes.md ./_post/pandas.md
+cp /Users/rocky/School/Obsidian/github-blog/leetcode/notes.md ./_post/leetcode.md
+rsync -av --delete /Users/rocky/School/Obsidian/github-blog/9517/images ./assets/images/comp9517/
 echo "success"
