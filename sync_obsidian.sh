96d0636a02f7f933a7571850fa3cf6dc3746f566
rocky <rocky.xyzhou@gmail.com>
2025-07-07 02:35:25 +1000

    Update sync_obsidian.sh

--- sync_obsidian.sh
+++ sync_obsidian.sh
@@ -5,4 +5,11 @@ cp /Users/rocky/School/Obsidian/github-blog/9517/notes.md ./_posts/comp9517.md
 cp /Users/rocky/School/Obsidian/github-blog/pandas/notes.md ./_posts/pandas.md
 cp /Users/rocky/School/Obsidian/github-blog/leetcode/notes.md ./_posts/leetcode.md
 rsync -av --delete /Users/rocky/School/Obsidian/github-blog/9517/images ./assets/images/comp9517/
+
+echo "sync complete, start submit Git..."
+
+git add .
+git commit -m "update"
+git push
+
 echo "success"
